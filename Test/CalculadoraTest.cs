e6ffc7bcc86b0b4aecd7040d140601c104703ac0
CristianS09 <redentorking90@gmail.com>
2024-04-22 20:34:00 -0300

    Primeiro commit

---
+++ Test/CalculadoraTest.cs
@@ -0,0 +1,42 @@
+using Console.Models;
+
+namespace test;
+
+public class CalculadoraTest
+{
+
+    public CalculadoraTest()
+    {
+        _cal = new Calculadora();
+    }
+    
+    private Calculadora _cal { get; set; }
+
+    [Fact]
+    public void TesteSoma()
+    {
+        var resultadoSuma = _cal.Somar(10,20);
+        Assert.Equal(30,resultadoSuma);
+    }
+
+    [Fact]
+    public void TesteSub()
+    {
+        var resultadoSub = _cal.Subtrair(10,5);
+        Assert.Equal(5,resultadoSub);
+    }
+
+      [Fact]
+    public void TesteDiv()
+    {
+        var resultadoDiv = _cal.Dividir(10,2);
+        Assert.Equal(5,resultadoDiv);
+    }
+    [Fact]
+    public void TesteMult()
+    {
+        var resultadoMult = _cal.Multiplicar(10,2);
+        Assert.Equal(20,resultadoMult);
+    }
+   
+}
